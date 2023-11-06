7f5b91faf8d763fc9c3f82d46804ffc92dc92529
ejooss@chungbuk.ac.kr <ejooss@chungbuk.ac.kr>
2023-11-06 10:12:03 +0900

    [기능 개발] 1단계

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,7 +1,31 @@
 package com.ll;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("== 명언 앱 ==");
+
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            System.out.print("명령) ");
+            String command = sc.readLine();
+
+            switch (command) {
+
+                case "종료":
+                    return;
+                    
+
+            }
+
+
+        }
+
+
     }
 }
